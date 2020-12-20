69b60a162e419431143815943d0488f44245508c
Vova <vovausatov95@gmail.com>
2020-12-20 19:58:16 +0300

    spread and es8

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
 "use strict"
 
-const str = "teSt";
+// let a = 5,
+//     b = a;
 
-// console.log(str[2] = 'd');
+// b = b + 5;
 
-// console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+// console.log(b);
+// console.log(a);
 
-const fruit = "Some fruit";
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
-console.log(fruit.indexOf("q"));
+// const copy = obj; //Ссылку
 
+// copy.a = 10;
 
-const logg = "Hello world";
+// console.log(copy);
+// console.log(obj);
 
-// console.log(logg.slice(-5, -1));
+function copy(mainObj) {
+    let objCopy = {};
 
-// console.log(logg.substring(6, 11));
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
 
+    return objCopy;
+}
 
-console.log(logg.substr(6, 5));
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y:4
+    }
+};
 
+const newNumbers = copy(numbers);
 
-const num = 12.2;
-console.log(Math.round(num));
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
+// console.log(newNumbers);
+// console.log(numbers);
 
-const test = "12.2px";
-// console.log(parseInt(test));
-console.log(parseFloat(test));
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = (Object.assign({}, add));
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adsasfd'
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array]; 
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
